fa0c13da99d1e9d748210c67bde8544d146d9fb2
Cosophia <577547955@qq.com>
2025-08-24 01:02:27 +0800

    更新了循环双链表

--- 数据结构学习/循环双链表.c
+++ 数据结构学习/循环双链表.c
@@ -3,12 +3,12 @@
 
 typedef struct LoopDLode {
 	int data;
-	struct LoopDLode *prior,*next;
-}LoopDLode,*LoopDLinklist;
+	struct LoopDLode* prior, * next;
+}LoopDLode, * LoopDLinklist;
 
 LoopDLinklist Initate(LoopDLinklist L) {
-	L=(LoopDLode*)malloc(sizeof(LoopDLode));
-	if (L==NULL)
+	L = (LoopDLode*)malloc(sizeof(LoopDLode));
+	if (L == NULL)
 	{
 		return NULL;
 	}
@@ -21,19 +21,19 @@ LoopDLinklist Initate(LoopDLinklist L) {
 	return L;
 }
 
-int LoopDLodeinsert(LoopDLinklist L,int index,int value) {
+int LoopDLodeinsert(LoopDLinklist L, int index, int value) {
 	LoopDLinklist p = L;
 	int count = 0;
-	while (p->next!=L && count<index-1)
+	while (p->next != L && count < index - 1)
 	{
 		p = p->next;
 		count++;
 	}
-	if (p==NULL)
+	if (p == NULL)
 	{
 		return 0;
 	}
-	LoopDLinklist s=(LoopDLinklist)malloc(sizeof(LoopDLode));
+	LoopDLinklist s = (LoopDLinklist)malloc(sizeof(LoopDLode));
 	if (s == NULL)
 	{
 		return 0;
@@ -51,21 +51,67 @@ int LoopDLodeinsert(LoopDLinklist L,int index,int value) {
 }
 
 void search(LoopDLinklist L) {
-	LoopDLinklist p = L->next;
-	while (p->next != L )
+	LoopDLinklist p = L;
+	while (p->next != L)
 	{
-		printf("%d  ",p->data);
 		p = p->next;
+		printf("%d  ", p->data);
+
 	}
+
+
+}
+
+int LoopDLodedelete(LoopDLinklist L, int index)
+{
+	LoopDLinklist p = L;
+	if (p == NULL)
+	{
+		return 0;
+	}
+	int count = 0;
+	while (p->next != L && count < index - 1)
+	{
+		p = p->next;
+		count++;
+	}
+	LoopDLinklist s = p->next;
+	p->next = s->next;
+	s->next->prior = s->prior;
+	free(s);
+	return 1;
+}
+
+
+
+LoopDLinklist GetElem(LoopDLinklist L, int index) {
+	if (index < 1)
+	{
+		return NULL;
+	}
+	LoopDLinklist p = L->next;
+	int count = 0;
+	while (count < index - 1)
+	{
+		p = p->next;
+		count++;
+	}
+	return p;
 }
 
 int main() {
 	LoopDLinklist Li = NULL;
 	LoopDLinklist L = Initate(Li);
-	LoopDLodeinsert(L,1,10);
+	LoopDLodeinsert(L, 1, 10);
 	LoopDLodeinsert(L, 2, 20);
 	LoopDLodeinsert(L, 3, 30);
 	LoopDLodeinsert(L, 4, 40);
+	printf("\n");
 	search(L);
+	LoopDLodedelete(L, 1);
+	printf("\n");
+	search(L);
+	printf("\n");
+	printf("%d", GetElem(L, 3)->data);
 	return 0;
 }
